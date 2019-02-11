6f3921e89177d71040b1147ae8fb74c519edbb2f
TheMysticMan <edolimburg@gmail.com>
2019-02-11 22:54:44 +0100

    fix copy job

--- brigade.js
+++ brigade.js
@@ -2,19 +2,32 @@ const { events, Job, Group } = require("brigadier");
 
 const shareFolder = "/mnt/brigade/share";
 const areaName = "gelderland";
-const downloadUrl = `europe/netherlands/${areaName}-latest.osm.pbf`
+const downloadUrl = `europe/netherlands/${areaName}-latest.osm.pbf`;
 const pbfFilePath = `${shareFolder}/${areaName}-latest.osm.pbf`;
 const osrmFilePath = `${shareFolder}/${areaName}-latest.osrm`;
 
-events.on('exec', (e,p) => {
-    jobs.importData(e,p);
+events.on('exec', (e, p) => {
+    try {
+        Group.runEach(
+            [
+                jobs.importData(e, p),
+                jobs.copyOsrmData(e, p)
+            ])
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    catch (err) {
+        console.log(err);
+    };
 });
 
 const jobs = {
-    importData: (e,p,env) => {
-        const job = new Job("import-osrm-data", "osrm-backend");
+    importData: (e, p) => {
+        const job = new Job("import-osrm-data", "osrm/osrm-backend");
         job.storage.enabled = true;
         job.tasks = [
+            `apt update && apt install curl -y`,
             `curl http://download.geofabrik.de/${downloadUrl} -o ${pbfFilePath}`,
             `osrm-extract -p /opt/car.lua ${pbfFilePath}`,
             `osrm-partition ${osrmFilePath}`,
@@ -23,8 +36,8 @@ const jobs = {
         return job;
     },
 
-    copyOsrmData: (e,p,env) => {
-        const copyJob = new Job('kubectl-copy-tiles', 'lachlanevenson/k8s-kubectl');
+    copyOsrmData: (e, p) => {
+        const copyJob = new Job('kubectl-copy-osrm-data', 'lachlanevenson/k8s-kubectl');
         copyJob.storage.enabled = true;
         copyJob.env = {
             BUILD_ID: e.buildID
@@ -34,6 +47,6 @@ const jobs = {
             `sh /src/copy-data.sh`
         ];
 
-        return copyJob.run();
+        return copyJob;
     }
 }
